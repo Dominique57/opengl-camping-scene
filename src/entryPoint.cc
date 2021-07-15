0498a1189f2afb9902f1e40a915c53b4092c22c4
Lea Masselles <leam@perion.com>
2021-07-15 11:58:10 +0200

    [OBJECT] First commit

--- src/entryPoint.cc
+++ src/entryPoint.cc
@@ -121,22 +121,29 @@ int run() {
 
     // build and compile shaders
     // -------------------------
-    auto* program = program::make_program_path("vert/obj_vertex_shader.glsl", "frag/obj_fragment_shader.glsl");
-    if (!program->isready()) {
-        std::cerr << "Failed to build shader :\n" << program->getlog() << '\n';
-        delete program;
+    auto* grass_shader = program::make_program_path("vert/obj_vertex_shader.glsl", "frag/obj_fragment_shader.glsl");
+    if (!grass_shader->isready()) {
+        std::cerr << "Failed to build shader :\n" << grass_shader->getlog() << '\n';
+        delete grass_shader;
         return 1;
     }
-    program->use();
+
+    auto* tree_shader = program::make_program_path("vert/obj_vertex_shader.glsl", "frag/obj_fragment_shader.glsl");
+    if (!tree_shader->isready()) {
+        std::cerr << "Failed to build shader :\n" << tree_shader->getlog() << '\n';
+        delete tree_shader;
+        return 1;
+    }
 
     // load models
     // -----------
-    Model ourModel("textures/backpack/backpack.obj");
-    Model robot("textures/nanosuit/nanosuit.blend");
+    Model grass("textures/grass/10450_Rectangular_Grass_Patch_v1_iterations-2.obj");
+    Model tree("textures/tree/TreeSet2/TreeSet.blend");
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window)) {
-        program->setUniformMat4("transform_matrix", camera.getTransform(), false);
+        grass_shader->setUniformMat4("transform_matrix", camera.getTransform(), false);
+        tree_shader->setUniformMat4("transform_matrix", camera.getTransform(), false);
 //        program->setUniformVec3("cameraPos", camera.viewCameraPos(), false);
         skyboxShader->setUniformMat4("transform_matrix", camera.getTransform(), true);
 
@@ -150,13 +157,22 @@ int run() {
         skybox.draw();
 
         glm::mat4 model = glm::mat4(1.0f);
-        model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f)); // translate it down so it's at the center of the scene
-        model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));	// it's a bit too big for our scene, so scale it down
+        model = glm::translate(model, glm::vec3(0.0f, -20.0f, 0.0f)); // translate it down so it's at the center of the scene
+        model = glm::scale(model, glm::vec3(0.2f, 0.2f, 0.2f));	// it's a bit too big for our scene, so scale it down
+        model = glm::rotate(model, glm::radians(-90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
         TEST_OPENGL_ERROR()
-        program->setUniformMat4("model", model, true);
-        program->use();
-        ourModel.draw(program);
-        robot.draw(program);
+        grass_shader->setUniformMat4("model", model, true);
+        grass_shader->use();
+        grass.draw(grass_shader);
+
+        glm::mat4 model_tree = glm::mat4(1.0f);
+        model_tree = glm::translate(model_tree, glm::vec3(0.0f, -18.8f, 0.0f)); // translate it down so it's at the center of the scene
+        model_tree = glm::scale(model_tree, glm::vec3(1.0f, 1.0f, 1.0f));	// it's a bit too big for our scene, so scale it down
+        model_tree = glm::rotate(model_tree, glm::radians(-90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+        TEST_OPENGL_ERROR()
+        tree_shader->setUniformMat4("model", model_tree, true);
+        tree_shader->use();
+        tree.draw(tree_shader);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
@@ -166,7 +182,8 @@ int run() {
     }
 
     glfwTerminate();
-    delete program;
+    delete tree_shader;
+    delete grass_shader;
     delete skyboxShader;
     return 0;
 }
